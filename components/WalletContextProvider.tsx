9bb4fa10bb0f855091986b62787b20a6f4eb087f
sincerelyyyash <yta0088@gmail.com>
2024-09-01 23:23:14 +0530

    Wallet content provider added

---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,32 @@
+
+import React, { useMemo, FC } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+} from "@solana/wallet-adapter-react-ui";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+export const WalletContextProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [
+    new UnsafeBurnerWalletAdapter(),
+  ], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
